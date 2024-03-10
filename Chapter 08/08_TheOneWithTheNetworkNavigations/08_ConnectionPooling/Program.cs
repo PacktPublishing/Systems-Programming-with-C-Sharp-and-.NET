a8e1e2172c3e27846508bdafa1d2a208320c0545
Dennis Vroegop <dennis@vroegop.org>
2024-03-10 16:22:47 +0100

    Added poolings

---
+++ Chapter 08/08_TheOneWithTheNetworkNavigations/08_ConnectionPooling/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Sockets;
+using _08_ConnectionPooling;
+
+await using var connectionPool = new TcpClientConnectionPool();
+
+TcpClient? myConnection = connectionPool.GetConnection();
+try
+{
+    var myBuffer = "Hello, World!"u8.ToArray();
+    // Use the connection
+    await myConnection.Client.SendAsync(myBuffer);
+}
+finally
+{
+    connectionPool.ReturnConnection(myConnection);
+}
