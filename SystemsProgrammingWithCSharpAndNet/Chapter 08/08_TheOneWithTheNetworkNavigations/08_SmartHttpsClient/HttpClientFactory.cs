801e417f49633f3fb3f05f0dbc4f2ba600c24ee4
Dennis Vroegop <dennis.vroegop@ibv.ch>
2024-03-11 11:46:39 +0100

    Added smart HttpClient factory

---
+++ SystemsProgrammingWithCSharpAndNet/Chapter 08/08_TheOneWithTheNetworkNavigations/08_SmartHttpsClient/HttpClientFactory.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+
+namespace _08_SmartHttpsClient
+{
+    internal static class HttpClientFactory
+    {
+        public static HttpClient? Instance
+        {
+            get
+            {
+                if (_instance == null) CreateInstance();
+                return _instance;
+            }
+        }
+        private static HttpClient? _instance;
+
+        private static void CreateInstance()
+        {
+            var handler = new HttpClientHandler()
+            {
+                UseCookies = true,
+                CookieContainer = new CookieContainer(),
+                UseProxy = false
+            };
+
+            _instance = new HttpClient(handler);
+            _instance.DefaultRequestHeaders.Clear();
+            _instance.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _instance.DefaultRequestHeaders.Add("User-Agent", "SystemProgrammersApp");
+            _instance.Timeout = TimeSpan.FromSeconds(5);
+        }
+    }
+}
---
+++ SystemsProgrammingWithCSharpAndNet/Chapter 08/08_TheOneWithTheNetworkNavigations/08_SmartHttpsClient/Program.cs
@@ -0,0 +1,8 @@
+﻿using _08_SmartHttpsClient;
+using ExtensionLibrary;
+
+"Getting data from the server...".Dump(ConsoleColor.Cyan);
+var client = HttpClientFactory.Instance;
+var data = await client.GetStringAsync(
+    "https://jsonplaceholder.typicode.com/posts");
+$"Received: {data}".Dump(ConsoleColor.Yellow);
