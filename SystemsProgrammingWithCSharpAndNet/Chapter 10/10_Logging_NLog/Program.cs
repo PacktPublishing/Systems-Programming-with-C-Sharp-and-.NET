d6a857241537390f54472997167ab6889af48991
Dennis Vroegop <dennis@vroegop.org>
2024-04-14 16:57:36 +0200

    Add more logging

--- SystemsProgrammingWithCSharpAndNet/Chapter 10/10_Logging_NLog/Program.cs
+++ SystemsProgrammingWithCSharpAndNet/Chapter 10/10_Logging_NLog/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NLog;
+using NLog.Config;
+
+LogManager.Configuration =
+    new XmlLoggingConfiguration(
+        "NLog.config"
+    );
+
+try
+{
+    var logger = LogManager.GetCurrentClassLogger();
+    var otherLogger  = LogManager.GetLogger("OtherLogger");
+
+    while (true)
+    {
+        logger.Trace("This is a trace message");
+        logger.Debug("This is a debug message");
+        logger.Info("Application started");
+        logger.Warn("This is a warning message");
+        logger.Error("This is an error message");
+        logger.Fatal("This is a fatal error message");
+
+        otherLogger.Info("This is a message from the other logger");
+        await Task.Delay(1000);
+    }
+}
+finally
+{
+    LogManager.Shutdown();
+}
