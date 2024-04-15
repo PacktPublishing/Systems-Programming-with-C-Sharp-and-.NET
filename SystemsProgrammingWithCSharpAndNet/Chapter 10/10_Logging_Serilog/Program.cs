ff253fb83749efefee953e51487d13009283b081
Dennis Vroegop <dennis.vroegop@ibv.ch>
2024-04-15 16:45:18 +0200

    Cleaning up Serilog

--- SystemsProgrammingWithCSharpAndNet/Chapter 10/10_Logging_Serilog/Program.cs
+++ SystemsProgrammingWithCSharpAndNet/Chapter 10/10_Logging_Serilog/Program.cs
@@ -1,46 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Runtime.CompilerServices;
-using _10_Logging;
-using ExtensionLibrary;
-using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Formatting.Json;
 
-"Welcome to the wonderful world of logging!".Dump(ConsoleColor.Cyan);
-
-// Setup the logger
-Log.Logger = new LoggerConfiguration()
+var logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console()
-    .WriteTo.File(@"c:\temp\log.txt", rollingInterval: RollingInterval.Day)
-    .WriteTo.Seq("http://localhost:5341/")
+    .WriteTo.File(
+        new JsonFormatter(".", true),
+        "logs\\log.txt",
+        rollingInterval: RollingInterval.Day)
     .CreateLogger();
-
-// Set up DI
-DependencyContainer.Setup();
-
-var logger = DependencyContainer.Resolve<ILogger<Program>>();
-if(logger == null)
-    throw new NullReferenceException("Logger is null");
 
 try
 {
-
-    logger.LogInformation("Hello, world!");
-    logger.LogWarning("This is a warning");
-    logger.LogError("This is an error");
-
-    logger.LogInformation("This is a structured log message with {property1} and {property2}", 1, 2);
-    throw new Exception("Something went wrong");
-}
-catch (Exception ex)
-{
-    logger.LogError(ex, "An error occurred");
+    logger.Verbose("This is verbose");
+    logger.Debug("This is debug");
+    logger.Information("This is information");
+    logger.Warning("This is warning");
+    logger.Error("This is error");
+    logger.Fatal("This is fatal");
 }
 finally
 {
-    Log.CloseAndFlush();
+    await Log.CloseAndFlushAsync();
 }
-
-Console.WriteLine("Press any key to exit");
-Console.ReadKey();
