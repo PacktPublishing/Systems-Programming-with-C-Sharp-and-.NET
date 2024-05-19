e8ed69e3c95f2654afbbca4fbbd3f249fe260785
Dennis Vroegop <dennis@vroegop.org>
2024-05-19 15:33:17 +0200

    Restructured

--- SystemsProgrammingWithCSharpAndNet/Chapter 11/11_MultiThreaded/Program.cs
+++ SystemsProgrammingWithCSharpAndNet/Chapter 11/11_MultiThreaded/Program.cs
@@ -1,3 +1,11 @@
-﻿using ExtensionLibrary;
+﻿using _11_MultiThreaded;
+using ExtensionLibrary;
 
-"Starting the threads".Dump(ConsoleColor.Cyan);
+//Deadlock dl = new Deadlock();
+//dl.Run();
+
+VariableCheck vc = new VariableCheck();
+vc.Run();
+
+"Waiting...".Dump(ConsoleColor.Cyan);
+Console.ReadLine();
