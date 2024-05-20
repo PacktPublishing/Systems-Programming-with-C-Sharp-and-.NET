9a3c00d258b40a87028e5647605f2ba6b283f62b
Dennis Vroegop <dennis@vroegop.org>
2024-05-20 17:25:36 +0200

    More changes

---
+++ SystemsProgrammingWithCSharpAndNet/Chapter 11/11_Benchmark/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+using _11_Benchmark;
+using BenchmarkDotNet.Running;
+
+//var mt = new ModuleTesters();
+//mt.TestMultiplicationAndDivisionOnGpu(12345, 1000000);
+var summary = BenchmarkRunner.Run<ModuleTesters>();
--- SystemsProgrammingWithCSharpAndNet/Chapter 11/11_Profiling/PrimeCalculator.cs
+++ SystemsProgrammingWithCSharpAndNet/Chapter 11/11_Profiling/PrimeCalculator.cs
@@ -1,57 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using ExtensionLibrary;
 
-namespace _11_Profiling
+namespace _11_Profiling;
+
+internal class PrimeCalculator
 {
-    internal class PrimeCalculator
+    public void Run()
     {
-        public void Run()
-        {
-            var limit = 100000;
-            
-                Stopwatch stopwatch = Stopwatch.StartNew();
-                long sum = SumOfPrimes(limit);
-                stopwatch.Stop();
+        var limit = 100000;
 
-                $"Sum of primes up to {limit}: {sum}".Dump(ConsoleColor.Cyan);
-                $"Time taken: {stopwatch.ElapsedMilliseconds} ms".Dump(ConsoleColor.Cyan);
+        var stopwatch = Stopwatch.StartNew();
+        var sum = SumOfPrimes(limit);
+        stopwatch.Stop();
 
-        }
+        $"Sum of primes up to {limit}: {sum}".Dump();
+        $"Time taken: {stopwatch.ElapsedMilliseconds} ms".Dump();
+    }
 
-        long SumOfPrimes(int limit)
-        {
-            long sum = 0;
-            for (int i = 2; i <= limit; i++)
-            {
-                if (IsPrime(i))
-                {
-                    sum += i;
-                }
-            }
+    private long SumOfPrimes(int limit)
+    {
+        long sum = 0;
+        for (var i = 2; i <= limit; i++)
+            if (IsPrime(i))
+                sum += i;
 
-            return sum;
-        }
+        return sum;
+    }
 
-        bool IsPrime(int number)
-        {
-            if (number < 2) return false;
-            if(number == 2) return true;
-            if(number % 2 == 0) return false;
-            var upperLoad = Math.Sqrt(number);
-            for (int i = 2; i <= upperLoad; i++) // Inefficiency: redundant check
-            {
-                if (number % i == 0)
-                {
-                    return false;
-                }
-            }
+    private bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+        for (var i = 2; i <= Math.Sqrt(number); i++)
+            if (number % i == 0)
+                return false;
 
-            return true;
-        }
+        return true;
     }
 }
