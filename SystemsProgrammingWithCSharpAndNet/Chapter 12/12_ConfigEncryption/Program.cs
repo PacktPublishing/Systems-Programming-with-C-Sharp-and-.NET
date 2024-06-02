e6fc75c5735a8fdd24c7fe973884c35bc9369c21
Dennis Vroegop <dennis@vroegop.org>
2024-06-02 14:58:37 +0200

    Added sample with enryption and decryption

---
+++ SystemsProgrammingWithCSharpAndNet/Chapter 12/12_ConfigEncryption/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using ExtensionLibrary;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddDataProtection();
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var dataProtector = serviceProvider.GetDataProtector("MySecureData");
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json",
+        false,
+        true)
+    .Build();
+
+var secretSection = configuration.GetSection("MySecretSettings");
+
+var json = File.ReadAllText("appsettings.json");
+
+
+foreach (var key in secretSection.GetChildren())
+{
+    var originalValue = key.Value;
+    var encryptedValue = dataProtector.Protect(originalValue);
+    var oldValue = $"\"{key.Key}\": \"{originalValue}\"";
+    var newValue = $"\"{key.Key}\": \"{encryptedValue}\""; 
+    json = json.Replace(oldValue, newValue);
+}
+
+File.WriteAllText("appsettings.json", json);
+
+// Force reloading.
+configuration.Reload();
+var encryptedSection = configuration.GetSection("MySecretSettings");
+var someSecretValue = encryptedSection["MySecretSetting1"];
+var decryptedValue = dataProtector.Unprotect(someSecretValue);
+$"Encrypted value was: {someSecretValue}\nDecrypted this becomes: {decryptedValue}".Dump();
--- SystemsProgrammingWithCSharpAndNet/Chapter 12/12_SecureStrings/Program.cs
+++ SystemsProgrammingWithCSharpAndNet/Chapter 12/12_SecureStrings/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Build configuration
+
+using ExtensionLibrary;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.DataProtection;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddDataProtection();
+var serviceProvider = serviceCollection.BuildServiceProvider();//(new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true });
+
+var dataProtector = serviceProvider.GetDataProtector("MySecureData");
+
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var json = File.ReadAllText("appsettings.json");
+
+var secretSection = configuration.GetSection("MySecretSettings");
+foreach(var key in secretSection.GetChildren())
+{
+    var originalValue = key.Value;
+    var encryptedValue = dataProtector.Protect(originalValue);
+    var oldValue = $"\"{key.Key}\": \"{originalValue}\"";
+    var newValue = $"\"{key.Key}\": \"{encryptedValue}\"";
+    json = json.Replace(oldValue, newValue);
+}
+
+File.WriteAllText("appsettings.json", json);
+
