11150093e805c38209bbd4f6ed2c3c2dbb2f0295
Dennis Vroegop <dennis.vroegop@ibv.ch>
2024-06-03 11:03:53 +0200

    Merging

---
+++ SystemsProgrammingWithCSharpAndNet/Chapter 12/12_SecureStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Security;
+using ExtensionLibrary;
+
+var normalString = "This is a string";
+
+SecureString secureString = new SecureString();
+for (var index = 0; index < normalString.Length; index++)
+{
+    var c = normalString[index];
+    secureString.AppendChar(c);
+}
+
+
+"Done.".Dump();
+Console.ReadLine();
+
+$"Just for completeness: {normalString}".Dump();
+$"And this one: {secureString}".Dump();
