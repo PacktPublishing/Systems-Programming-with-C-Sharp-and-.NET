1c13d135d2e1d2edac15ebc4895aa262fab1296d
Dennis Vroegop <dennis.vroegop@ibv.ch>
2024-06-20 15:45:33 +0200

    Updated packages

---
+++ SystemsProgrammingWithCSharpAndNet/Chapter 13/13_CustomAction/ProjectInstaller.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace _13_CustomAction
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+            var secretsInstaller = new SecretsInstaller();
+            Installers.Add(secretsInstaller);
+        }
+    }
+}
