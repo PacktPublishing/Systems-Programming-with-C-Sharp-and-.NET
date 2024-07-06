98d634c2cd4c5f00a627fc31f17942da24f4a74b
Dennis Vroegop <dennis@vroegop.org>
2024-07-06 15:56:00 +0200

    Fixed Sln 01

--- SystemsProgrammingWithCSharpAndNet/Chapter01/MyAwesomeCalculator/Program.cs
+++ SystemsProgrammingWithCSharpAndNet/Chapter01/MyAwesomeCalculator/Program.cs
@@ -1,10 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Starting the loop\n\n");
+using ExtensionLibrary;
+
+"Starting the loop\n\n".Dump();
 int i = 1;
 while (true)
 {
-    Console.WriteLine($"In the loop at {DateTime.Now.TimeOfDay} for loop {i++}");
+    $"In the loop at {DateTime.Now.TimeOfDay} for loop {i++}".Dump();
     Thread.Sleep(1000);
     if(i > 10)
         throw new Exception("Something weird happened...");
